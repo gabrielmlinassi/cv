5661d415604908a63a7e66c91ff4264b6a6de9b1
Gabriel <gabrielm.linassi@gmail.com>
2021-07-12 12:09:17 -0300

    updtes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const base =
+  process.env.NODE_ENV === "production"
+    ? "https://gabrielmlinassi.com"
+    : "http://localhost:4000";
+
 module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/gallery",
+          destination: `${base}/gallery`,
+        },
+      ],
+    };
   },
 };

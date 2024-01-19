81399f231ba5805bf18fcbf18c62139b114f13b4
Gabriel Marmitt <gabrielm.linassi@gmail.com>
2024-01-19 19:11:41 -0300

    cleanup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,20 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatAsMoney(value: number) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(value);
-}
-
-export function formatAsPercentage(value: number) {
-  return new Intl.NumberFormat("en-US", {
-    style: "percent",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(value / 100);
-}

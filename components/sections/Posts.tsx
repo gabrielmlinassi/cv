d3078c8a3600485e21f9f4343b39069cbb6e7ce0
Gabriel Marmitt <gabrielm.linassi@gmail.com>
2024-02-11 10:37:05 -0300

    fix console error and bump some deps version

--- components/sections/Posts.tsx
+++ components/sections/Posts.tsx
@@ -1,43 +1,27 @@
 import Link from "next/link";
 import { format } from "date-fns";
 
-import { Heading } from "../typography/Heading";
+import { Heading } from "@/components/typography/Heading";
+import { getAllPosts } from "@/lib/posts";
 
-const posts = [
-  {
-    title: "Using WebSockets in a Redux Application",
-    date: "01-02-2024",
-    url: "/websockets-in-redux",
-  },
-  {
-    title: "Understanding the GraphQL Type System",
-    date: "01-02-2024",
-    url: "/graphql-type-system",
-  },
-  {
-    title: "An Ode to Animorphs",
-    date: "01-02-2024",
-    url: "/animorphs",
-  },
-];
-
-export const Posts = () => {
+export const Posts = async () => {
+  const posts = await getAllPosts();
   return (
     <section className="container mt-5">
       <Heading level="h2">Latest Posts</Heading>
       <ol>
         {posts.map((post) => (
           <li
-            key={post.title}
+            key={post.frontmatter.title}
             className="border-b p-[6px] dark:border-[#2c2c2c] border-[#dddddd]"
           >
-            <Link href={post.url}>
+            <Link href={post.slug}>
               <div className="flex flex-col md:flex-row justify-between md:items-center group">
                 <p className="font-semibold antialiased text-base text-[#666666] md:text-[17px] dark:group-hover:text-white group-hover:underline group-hover:underline-offset-2 group-hover:decoration-2">
-                  {post.title}
+                  {post.frontmatter.description}
                 </p>
                 <time className="font-mono text-base text-[#666666] dark:text-[#5eddac] font-light">
-                  {format(post.date, "MMMM yyyy")}
+                  {format(post.frontmatter.publishedAt, "MMMM yyyy")}
                 </time>
               </div>
             </Link>
